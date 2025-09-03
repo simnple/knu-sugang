18ff198b7231707781cd3bcca87dcd06c0eac23d
Simnple <75520229+simnple@users.noreply.github.com>
2025-09-03 19:25:17 +0900

    code 500 추가

--- main.py
+++ main.py
@@ -27,11 +27,17 @@ while True:
             result = sugang.input_code(code)
             if result["code"] == 200:
                 print(f"[+] 보안 문자를 해결했습니다. | code: {code} | failCnt: {result['failCnt']}")
+            elif result["code"] == 500:
+                print(f"[-] 보안 문자를 잘못 입력했습니다. | failCnt: {result['failCnt']}")
             else:
-                print(f"[-] 보안 문자 해결을 실패했습니다. | {result}")
+                print(f"[-] 보안 문자 해결을 실패했습니다. | result: {result}")
 
         elif result["code"] == 204:
             print(f"[-] 해당 강의는 정원 초과 상태입니다.")
+
+        elif result["code"] == 500:
+            print(f"[-] 수강신청 기간이 끝났습니다.")
+            exit()
 
         elif result["code"] == 999:
             print(f"[-] 로그인을 재시도합니다.")
